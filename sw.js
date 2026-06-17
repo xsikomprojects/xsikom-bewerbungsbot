9b53b8226327898b80a8a42fb6f1efda33d4be51
xsikomprojects <xsikom.projects@gmail.com>
2026-06-17 18:22:13 +0200

    PWA verbessert fuer Play Store

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-const CACHE = 'xsikom-v1';
+const CACHE_NAME = 'xsikom-v2';
+const URLS_TO_CACHE = [
+    '/',
+    '/manifest.json',
+    '/static/icon-192.png',
+    '/static/icon-512.png'
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE).then((c) => c.addAll(['/']))
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
     );
 });
