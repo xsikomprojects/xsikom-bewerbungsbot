2ca60e9d4b44c8c10327b9d7219f14f88cc6ea93
xsikomprojects <xsikom.projects@gmail.com>
2026-06-24 19:20:20 +0200

    DB fix

--- webapp.py
+++ webapp.py
@@ -783,4 +783,4 @@ if __name__ == "__main__":
     print(f"  URL: http://localhost:5000")
     print("=" * 60)
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
